fc275aed3a38ecd076babff567e2b4ba27971235
CuervoTonto <brahians.mosquerav@uqvirtual.edu.co>
2025-10-05 10:26:01 -0500

    update: controller for product creation

---
+++ server/src/main/java/co/edu/uniquindio/comandera/infrastructure/spring/controllers/product/CreateProductController.java
@@ -0,0 +1,34 @@
+package co.edu.uniquindio.comandera.infrastructure.spring.controllers.product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.uniquindio.comandera.application.dto.product.CreateProductRequestDto;
+import co.edu.uniquindio.comandera.application.usecases.product.CreateProductUseCase;
+
+@RestController
+@RequestMapping(path = "/api/")
+public class CreateProductController
+{
+    @Autowired
+    private CreateProductUseCase createProduct;
+
+    @PostMapping(path = "product")
+    public ResponseEntity<?> createProduct(
+        @RequestBody
+        CreateProductRequestDto productCreationDto
+    ) {
+        try {
+            this.createProduct.execute(productCreationDto);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}

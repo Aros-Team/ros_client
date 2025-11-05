aa2a6d88c747cb3bac4e722a3ce6259beb24b458
Jorge-ivan-aa <jorgei.acostaa@uqvirtual.edu.co>
2025-11-05 00:07:28 -0500

    refactor: update deprecated ngif and ngfor

--- src/app/features/admin/manage/kitchen/kitchen.ts
+++ src/app/features/admin/manage/kitchen/kitchen.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { OrderService } from '@app/core/services/orders/order-service';
 import { OrderDetailsResponse } from '@app/shared/models/dto/orders/order-details-response.model';
@@ -11,6 +11,7 @@ import { OrderDetailsResponse } from '@app/shared/models/dto/orders/order-detail
   styles: ``
 })
 export class Kitchen implements OnInit {
+  private orderService: OrderService = inject(OrderService);
   title = 'Cocina';
   description = 'Gestión de las operaciones de cocina y preparación de pedidos';
 
@@ -18,8 +19,6 @@ export class Kitchen implements OnInit {
   error: string | null = null;
   pendingOrders: OrderDetailsResponse[] = [];
   processing = new Set<number>();
-
-  constructor(private orderService: OrderService) { }
 
   ngOnInit(): void {
     this.fetchOrders();
